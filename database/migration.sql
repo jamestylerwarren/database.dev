6f929fbec823d2173ae5abc26a34fdb3c079f1b8
Tyler Warren <warrejt@yahoo.com>
2016-08-01 10:33:05 -0500

    starting exercise - added databases to mysql, added foreign keys, added search based on league given

--- database/migration.sql
+++ database/migration.sql
@@ -12,3 +12,22 @@ CREATE TABLE teams (
   PRIMARY KEY (id)
 );
 
+CREATE TABLE players (
+  id       INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  team_id  INT UNSIGNED NOT NULL,
+  name     VARCHAR(255) NOT NULL,
+  position VARCHAR(50),
+  PRIMARY KEY (id), 
+  FOREIGN KEY (team_id) REFERENCES teams (id)
+);
+
+CREATE TABLE games (
+  visitor_team_id   INT UNSIGNED NOT NULL,
+  local_team_id     INT UNSIGNED NOT NULL,
+  visitor_team_runs INT UNSIGNED NOT NULL,
+  local_team_runs   INT UNSIGNED NOT NULL,
+  game_date         DATETIME     NOT NULL,
+  PRIMARY KEY (visitor_team_id, local_team_id, game_date), 
+  FOREIGN KEY (visitor_team_id) REFERENCES teams (id), 
+  FOREIGN KEY (local_team_id) REFERENCES teams (id)
+);
